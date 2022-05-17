7770930b5b4d5cf9776707a16bc811294ca10142
Chanponleusophea HO <>
2022-05-17 02:58:01 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Second-lesson-1/calculation.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int x;
+    x =;
+    float y;
+    y=(float)(pow(2.0,2));
+    float z=(x+y)/(x+1);
+    float a=sqrt(z);
+    printf("%f",a);
+    return 0;
+}
